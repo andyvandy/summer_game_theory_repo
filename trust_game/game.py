431238c49581c6a7cf23bc2029a385e3c5b610e0
StuartJSquires <StuartJSquires@users.noreply.github.com>
2015-06-03 14:08:23 -0400

    fixed game.py for new model

--- trust_game/game.py
+++ trust_game/game.py
@@ -2,25 +2,26 @@ import random as r
 import numpy as np
 from agent_class import *
 
-def play_game(agent_1, agent_2, b, turns, reset):
+def play_game(agent_1, agent_2, b, c, turns, swap):
     """Plays an iterated trust game between Agent 1 and Agent 2. Agent 1 plays
     as investor first.
 
     Args:
         agent_1, agent_2: two agents to play together.
-        b: the multiplier for the transfer
+        b: the multiplier for the first transfer
+        c: the multiplier for the return transfer
         turns: the number of turns to be played
-        reset: a boolean determining whether or not the balance is reset for the
-            players each turn
+        swap: a boolean determining whether or not the players swap roles
+            between turns
 
     Returns:
-        game_stats: a tuple containing (agent 1 score, 
-                                        agent 2 score, 
+        game_stats: a tuple containing (agent_1_avg_score, 
+                                        agent_2_avg_score, 
                                         agent_1_avg_gifot, 
                                         agent_2_avg_gift)
     """
 
-    agent_1_balance = 100.
+    agent_1_balance = 10.
     agent_2_balance = 0.
     agent_1_scores = np.zeros(turns)
     agent_2_scores = np.zeros(turns)
@@ -31,42 +32,35 @@ def play_game(agent_1, agent_2, b, turns, reset):
     turn_stats = np.zeros((turns, 4))
 
     for turn in range(turns):
-        if turn % 2 == 0:
+        if swap and turn % 2 == 1:
+            turn_stats[turn] = play_turn(agent_2, agent_1, agent_2_balance,
+                                         agent_1_balance, agent_2_history, 
+                                         agent_1_history, b, c, turn)
+
+            agent_1_scores[turn] = turn_stats[turn][1]
+            agent_2_scores[turn] = turn_stats[turn][0]
+            agent_1_gifts = turn_stats[turn][3]
+            agent_2_gifts = turn_stats[turn][2]
+        else:
             turn_stats[turn] = play_turn(agent_1, agent_2, agent_1_balance,
                                             agent_2_balance, agent_1_history, 
-                                            agent_2_history, b, turn)
-            agent_1_scores[turn] = turn_stats[turn][0]
-            agent_2_scores[turn] = turn_stats[turn][1]
-
-        else:
-            turn_stats[turn] = play_turn(agent_2, agent_1, agent_2_balance,
-                                            agent_1_balance, agent_2_history, 
-                                            agent_1_history, b, turn)
+                                            agent_2_history, b, c, turn)
 
             agent_1_scores[turn] = turn_stats[turn][0]
             agent_2_scores[turn] = turn_stats[turn][1]
+            agent_1_gifts = turn_stats[turn][2]
+            agent_2_gifts = turn_stats[turn][3]
 
-        agent_1_gifts[turn] = turn_stats[turn][2]
-        agent_1_gifts[turn] = turn_stats[turn][3]
-
-        if reset:
+        if swap:
             agent_1_balance, agent_2_balance = agent_2_balance, agent_1_balance
-        else:
-            agent_1_balance = agent_1_scores[turn]
-            agent_2_balance = agent_2_scores[turn]
 
     agent_1_avg_gift = np.mean(agent_1_gifts)
     agent_2_avg_gift = np.mean(agent_2_gifts)
+    agent_1_avg_score = np.mean(agent_1_scores)
+    agent_2_avg_score = np.mean(agent_2_scores)
 
-    if reset:
-        agent_1_score = np.sum(agent_1_scores)
-        agent_2_score = np.sum(agent_2_scores)
-    else:
-        agent_1_score = agent_1_balance
-        agent_2_score = agent_2_balance
-
-    game_stats = (agent_1_score, 
-                  agent_2_score, 
+    game_stats = (agent_1_avg_score, 
+                  agent_2_avg_score, 
                   agent_1_avg_gift, 
                   agent_2_avg_gift)
 
@@ -74,12 +68,13 @@ def play_game(agent_1, agent_2, b, turns, reset):
 
 
 def play_turn(investor, trustee, investor_balance, trustee_balance,
-               investor_history, trustee_history, b, turn):
+               investor_history, trustee_history, b, c, turn):
     """Plays a single turn of the trust game between investor and trustee.
 
     Args:
         investor, trustee: two agents to play together
         b: the multiplier for the first transfer
+        c: the multiplier for the return transfer
         investor_balance, trustee_balance: starting balances for the investor 
             and trustee
         turn: the current turn
@@ -91,23 +86,21 @@ def play_turn(investor, trustee, investor_balance, trustee_balance,
                                          trustee_gift_fraction)
     """
 
-    investor_gift_fraction = investor.gift(turn, trustee_history) 
-    investor_gift = investor_gift_fraction * investor_balance
-    investor_history[turn] = investor_gift / investor_balance
+    investor_gift = investor.gift(turn, trustee_history, type = 0)
+    investor_history[turn] = investor_gift
     investor_score = investor_balance - investor_gift
 
-    trustee_score = trustee_balance + investor_gift
-    trustee_gift_fraction = trustee.gift(turn, investor_history) 
-    trustee_gift = trustee_gift_fraction * trustee_score
-    trustee_history[turn] = trustee_gift / trustee_score
+    trustee_score = trustee_balance + (b * investor_gift)
+    trustee_gift = trustee.gift(turn, investor_history, type = 0)
+    trustee_history[turn] = trustee_gift
     trustee_score = trustee_score - trustee_gift
 
-    investor_score = investor_score + (b * trustee_gift)
+    investor_score = investor_score + (c * trustee_gift)
 
-    turn_stats = (investor_score, 
-                   trustee_score, 
-                   investor_gift_fraction, 
-                   trustee_gift_fraction)
+    turn_stats = (investor_score,
+                  trustee_score,
+                  investor_gift,
+                  trustee_gift)
 
     return turn_stats
 
--- trust_game/trust_config.py
+++ trust_game/trust_config.py
@@ -5,3 +5,4 @@ ROUNDS = 250 # Matchups per gen
 B = 3 # Multiplier for Investor -> Trustee
 C = 1 # Multiplier for Trustee -> Investor
 RESET = True # Whether or not balance is reset after each turn
+SWAP = False # Whether or not agents switch roles after each turn
