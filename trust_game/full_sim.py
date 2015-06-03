7c7b158878fd776ecc7e2a63afee9f579099639a
StuartJSquires <StuartJSquires@users.noreply.github.com>
2015-06-03 11:52:20 -0400

    Added multiple rounds per gen

--- trust_game/full_sim.py
+++ trust_game/full_sim.py
@@ -19,28 +19,28 @@ def main():
     avg_gift = np.zeros(GENERATIONS)
 
     for i in range(GENERATIONS):
-
-        # Sets the number of turns with an exponential distribution, with a 
-        # maximum at the 95th percentile
-        turns = min(int(round(r.expovariate(theta))) + 1, 
-                 int(-np.log(0.05) / theta)))
-        
         # Mutates the agents if necessary
-        if i != 0:
-            agent_list.sort(key = lambda x: x.score, reverse = True)
-            agent_list = mutate_agents(agent_list[:NUMBER_OF_AGENTS / 2], 
-                                       max_turns)
+            if i != 0:
+                agent_list.sort(key = lambda x: x.score, reverse = True)
+                agent_list = mutate_agents(agent_list[:NUMBER_OF_AGENTS / 2], 
+                                           max_turns)
 
-        for j in range(NUMBER_OF_AGENTS / 2):
-            game_stats = play_game(agent_list[j], agent_list[j + 1], b = B, 
-                                   turns = turns, reset = RESET)
-            agent_list[j].score = game_score[0]
-            agent_list[j + 1].score = game_score[1]
-            agent_list[j].avg_gift = game_score[2]
-            agent_list[j].avg_gift = game_score[3]
+        for j in range(ROUNDS):
+            # Sets the number of turns with an exponential distribution, with a 
+            # maximum at the 95th percentile
+            turns = min(int(round(r.expovariate(theta))) + 1, 
+                     int(-np.log(0.05) / theta)))
+            
+            for j in range(NUMBER_OF_AGENTS / 2):
+                game_stats = play_game(agent_list[j], agent_list[j + 1], b = B, 
+                                       turns = turns, reset = RESET)
+                agent_list[j].score = game_score[0]
+                agent_list[j + 1].score = game_score[1]
+                agent_list[j].avg_gift = game_score[2]
+                agent_list[j].avg_gift = game_score[3]
 
         if GENERATIONS % 25 == 0:
-            print GENERATIONS, "generations complete."
+                print GENERATIONS, "generations complete."
 
     print "The simulation has completed."
     print "Generating plot..."
