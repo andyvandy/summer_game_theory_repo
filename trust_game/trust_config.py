e9764214d33433721c08fb46f548f7857137687b
StuartJSquires <StuartJSquires@users.noreply.github.com>
2015-06-03 11:53:28 -0400

    added rounds parameter to config

--- trust_game/trust_config.py
+++ trust_game/trust_config.py
@@ -1,6 +1,7 @@
 W = 0.95 # Probability that the game goes on another turn
 NUMBER_OF_AGENTS = 100
 GENERATIONS = 100
+ROUNDS = 250 # Matchups per gen
 B = 3 # Multiplier for Investor -> Trustee
 C = 1 # Multiplier for Trustee -> Investor
 RESET = True # Whether or not balance is reset after each turn
