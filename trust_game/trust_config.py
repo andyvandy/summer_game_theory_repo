c9e054174d614f8b15e92f694a387d6a56519a51
StuartJSquires <StuartJSquires@users.noreply.github.com>
2015-06-12 14:28:42 -0400

    Corrected lie in config comment

--- trust_game/trust_config.py
+++ trust_game/trust_config.py
@@ -17,4 +17,5 @@ MEMORY = 4    # How far back the agents remember
 # (# breed, # live but not breed, # newcommers)
 MUTATION_PARAMS= (14,8,0)    
 
-# For all or nothing, set A = B, ENDOWMENT = (1, 0)
+# For all or nothing, set A your desired investor endowment, 
+# B = (A * the multiplier on that endowment), and ENDOWMENT = (1, 0) 
